2ab2d207e2d021b7eba9fb675d0d7a2981560e9b
Dan <70431670+Danielx2003@users.noreply.github.com>
2023-07-31 17:28:17 +0100

    getURLsFromHTML func and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,36 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody) //creates a document object model of the html file
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const link of linkElements) {
+        if (link.href.slice(0, 1) === '/') {
+            //relative
+            try {
+                const urlObj = new URL(`${baseURL}${link.href}`)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`err`)
+            }
+        } else {
+            //absolute
+            try {
+                const urlObj = new URL(link.href)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`err`)
+            }
+        }
+    }
+    console.log(urls)
+    return urls
+}
+
+
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostpath = `${urlObj.hostname}${urlObj.pathname}` //strips protocols
@@ -8,5 +41,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
